ad432dbf5dcca10e026d8216d01705dd52cd77a5
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-04-08 10:46:36 +0300

    Add menu

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 const menuButton = document.querySelector(".menu-button");
 
-const handleMenuButton = () => {
+const handleMenuButtonPress = () => {
   console.log("Menu button pressed!");
   const menuIcon = document.querySelector(".menu-icon");
+  const menu = document.querySelector(".menu");
   menuIcon.classList.toggle("active");
+  menu.classList.toggle("show");
 };
 
 const app = () => {
-  menuButton.addEventListener("click", handleMenuButton);
+  menuButton.addEventListener("click", handleMenuButtonPress);
 };
 
 export default app;
